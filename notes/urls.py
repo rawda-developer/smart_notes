9830685366d57e6625b01a1a12afe8b57f06bfdf
Rawda Yasser <rawda.developer@gmail.com>
2022-01-21 11:10:22 +0200

    update notes app: change function based views to class-based views + update urls

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from unicodedata import name
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('all/', views.notes_list, name='notes_list'),
-    path('<int:pk>', views.note_detail, name='notes_detail'),
+    path('all/', views.NotesList.as_view(), name='notes_list'),
+    path('<int:pk>', views.NoteDetail.as_view(), name='notes_detail'),
 
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Note
 
 
-def notes_list(request):
-    all_notes = Note.objects.all()
-    return render(request, 'notes/all_notes.html', {'all_notes': all_notes})
+class NotesList(ListView):
+    model = Note
+    context_object_name = 'all_notes'
+    template_name = 'notes/all_notes.html'
 
 
-def note_detail(request, pk):
-    try:
-        note = Note.objects.get(pk=pk)
-    except Note.DoesNotExist:
-        return render(request, 'notes/404.html', {})
-    return render(request, 'notes/note_detail.html', {'note': note})
+class NoteDetail(DetailView):
+    model = Note
+    template_name = 'notes/note_detail.html'
