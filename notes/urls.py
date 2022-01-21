7fb80f5d5c34a0b791c91fb1207d6aa924b7d7c5
Rawda Yasser <rawda.developer@gmail.com>
2022-01-21 10:36:13 +0200

    add note_detail view + template + url

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.notes_list, name='notes_list'),
-    
+    path('all/', views.notes_list, name='notes_list'),
+    path('<int:pk>', views.note_detail, name='notes_detail'),
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -5,3 +5,11 @@ from .models import Note
 def notes_list(request):
     all_notes = Note.objects.all()
     return render(request, 'notes/all_notes.html', {'all_notes': all_notes})
+
+
+def note_detail(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return render(request, 'notes/404.html', {})
+    return render(request, 'notes/note_detail.html', {'note': note})
