e4fc4856b37056323564d0ab5e37a657b6e5b77c
Rawda Yasser <rawda.developer@gmail.com>
2022-01-21 07:56:04 +0200

    pass date from home function to welcome template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 
 def home(request):
-    return render(request, 'home/welcome.html', {})
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
